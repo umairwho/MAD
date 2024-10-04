1d8134d2d9625316e6c5a2a6b97f327459a6f95d
Umair Asad <umair.asad5@hotmail.com>
2024-10-05 00:21:46 +0500

    task3.kt

---
+++ task3.kt
@@ -0,0 +1,19 @@
+fun main() {
+    
+    var score = 0;
+    println(score)
+    score += getPoints(10,1)
+    println(score)
+    score += getPoints(20,2)
+    println(score)
+    score += getPoints(-10,1)
+    println(score)
+    score += getPoints(5,3)
+    println(score)
+    score += getPoints(-15, 2)
+    
+}
+
+fun getPoints(basePoints: Int, boost: Int): Int {
+    return basePoints * boost
+}
